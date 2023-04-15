cba8e8596dc93bddc90d8352b386b759b3ffb73f
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-04-15 16:29:07 +0200

    find a Movies API and created filmCard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import Header from './components/header';
+import FilmCard from './components/filmCard';
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd8f3bdddfcmsh0d2102590538877p1b0578jsn04f9fd220639',
+        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
+      },
+    };
+
+    fetch(
+      'https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr',
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setFilms(response.d))
+      .catch((err) => console.error(err));
+  }, []);
+  console.log(films);
   return (
     <main>
       <Header />
+      {films && films.map((film, idx) => <FilmCard key={idx} props={film} />)}
     </main>
   );
 }
---
+++ src/components/filmCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function FilmCard({ props }) {
+  console.log(props);
+  return <div>cardfilm</div>;
+}
+
+export default FilmCard;
