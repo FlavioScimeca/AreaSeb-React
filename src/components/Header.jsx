a119daf51dee8ec14eed2909e01c02f7369ec641
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-04-15 18:54:07 +0200

    add in the Header, possibility to navigate with useNavigate() && installed @reduxjs/toolkit and redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FilmIcon } from '@heroicons/react/24/outline';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <div className="bg-zinc-300 flex justify-between items-center py-3 px-6">
-      <div>
+      <div onClick={() => navigate('/')}>
         <p className="text-xl font-semibold">VideoTeca</p>
       </div>
-      <div className="relative">
+      <div onClick={() => navigate('/wishlist')} className="relative">
         <FilmIcon className="h-10 w-10 p-1" />
         <div className="absolute top-6 left-6 p-3 bg-sky-700 rounded-full">
           <span className="absolute bottom-[2px] right-2">1</span>
